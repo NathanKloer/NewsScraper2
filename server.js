cccab78d61d28ac5cc3c4e16633ca515d8570c16
Nathan <nathan.kloer@gmail.com>
2019-02-15 00:34:52 -0500

    git update cleaning

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-var logger = require("morgan");
-var db = require("./models");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
